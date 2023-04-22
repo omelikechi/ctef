e90be86dd1fc90578761e7c27715d833b5197681
Omar Melikechi <omar.melikechi@gmail.com>
2023-04-22 13:33:44 -0400

    clean up

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="ctef",
-    version="0.0.1",
+    version="0.0.3",
     packages=find_packages(),
     description="Cayley transform ellipsoid fitting",
     long_description=long_description,
