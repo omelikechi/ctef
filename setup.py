d060a63085fd21360fb98b67a07307a284d99f9c
Omar Melikechi <omar.melikechi@gmail.com>
2023-04-21 22:06:48 -0400

    update

--- setup.py
+++ setup.py
@@ -1,19 +1,16 @@
-from os import path
 from setuptools import find_packages, setup
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name="nflows",
-    version="0.1dev",
+    name="ctep",
+    version="0.0.1",
     description="Cayley transform ellipsoid fitting",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/omelikechi/ctef",
     author="Omar Melikechi",
-    packages=find_packages(exclude=["tests"]),
     license="MIT",
     install_requires=[
         "matplotlib",
