8cd7e7e881802694a1d14d0fb14006f46a75f123
Omar Melikechi <omar.melikechi@gmail.com>
2025-02-07 13:52:55 -0500

    bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="ctef",
-    version="0.0.3",
+    version="0.0.4",
     packages=find_packages(),
     description="Cayley transform ellipsoid fitting",
     long_description=long_description,
